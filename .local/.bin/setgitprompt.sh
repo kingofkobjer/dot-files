2b1930fb390684ac05887a5a688d1ab57747fec9
KingOfKobjer <kingofkobjer@egmail.com>
2015-08-14 19:25:19 +0200

    added helper bash files

---
+++ .local/.bin/setgitprompt.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source ~/.bash-git-prompt/gitprompt.sh
---
+++ .local/.bin/setkokbjrtogit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git config --local user.email "kingofkobjer@egmail.com"
+git config --local user.name "KingOfKobjer"
